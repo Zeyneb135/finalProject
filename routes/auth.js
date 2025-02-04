895a4233fce6b9aabb7a082ad488d096cd342044
Zeyneb135 <zeynabeg-sp102@code.edu.az>
2025-02-05 00:46:58 +0400

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -8,8 +8,6 @@ router.post('/register', register)
 router.post('/login', login)
 
 export default router
-=======
 import express, { Router } from 'express' 
 
 const router = 
->>>>>>> 76ec7ad4ee737ce1deeb5cf5039f963c48464425
